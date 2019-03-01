9624b1f5b08b87cb5855ba0da4e9964ddd21b44f
aks-c <akselcakmak@gmail.com>
2019-03-01 17:09:40 +0000

    Cleanup

--- Q2.Basic Bidding Strategies/agent.py
+++ Q2.Basic Bidding Strategies/agent.py
@@ -1,6 +1,7 @@
 class Agent:
-    def __init__(self):
+    def __init__(self, id: int):
         self.clicks = 0
+        self.id = id
     
     def updateClicks(self, new_clicks: int):
         self.clicks += new_clicks
